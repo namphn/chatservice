7f77eb405e10124d00e2bbe36bbd486b522b74aa
namph-uet <hoangnamuet.vnu@gmail.com>
2020-09-25 21:23:48 +0700

    update

--- src/main/java/web/service/chat/config/WebSocketConfig.java
+++ src/main/java/web/service/chat/config/WebSocketConfig.java
@@ -12,13 +12,13 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
 
     @Override
     public void registerStompEndpoints(StompEndpointRegistry registry) {
-        registry.addEndpoint("/ws").setAllowedOrigins("http://localhost:3000").withSockJS();
+        registry.addEndpoint("/chat").setAllowedOrigins("**").withSockJS();
     }
 
     @Override
     public void configureMessageBroker(MessageBrokerRegistry registry) {
         registry.setApplicationDestinationPrefixes("/app");
-        registry.enableSimpleBroker("/queue/", "/topic/", "/user/");
+        registry.enableSimpleBroker("/queue", "/topic", "/user");
         registry.setUserDestinationPrefix("/user");
     }
 }
